92a9ce364005366b7ee304c06dfcffac81c631ff
Spencer Harston <spencer314@gmail.com>
2018-12-12 08:00:37 -0700

    Add Recipe class; add migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,7 +6,21 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    ingredients = db.Column(db.Text)
+    instructions = db.Column(db.Text)
+    timestamp = db.Column(db.Date, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Recipe {}>'.format(self.title)
